af8d81a41e240e228cc59bc8a703ef5587df00a5
zhangqiang <zhangqiang@cgali.com>
2020-06-30 12:07:06 +0800

    move addProvince function
    Signed-off-by: zhangqiang <zhangqiang@cgali.com>

--- src/App.js
+++ src/App.js
@@ -233,7 +233,7 @@ class App extends React.Component {
      
       tool.addTool("筑", Tools.addBuilding);
       tool.addTool("量",Tools.drawControl);
-      // tool.addTool("省",Tools.addProvince);
+      tool.addTool("省",Tools.addProvince);
 
 
     })
@@ -259,15 +259,7 @@ class App extends React.Component {
 
    
 
-    // tool.addTool("动", (e) => {
-    //   moveSin()
-    // })
 
-    tool.addTool("省", (e) => {
-      var me = this;
-      debugger;
-      addProvince1(me)
-    })
 
     
 
@@ -954,139 +946,6 @@ class App extends React.Component {
     }
 
 
-    function addProvince1(me) {
-      const provinceid = "province";
-
-      if (map.getLayer("province")) {
-        map.removeLayer('province').removeSource("province");
-        return;
-      }
-      var layers = map.getStyle().layers;
-      // var labelLayerId;
-      for (var i = 0; i < layers.length; i++) {
-        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-          app.labelLayerId = layers[i].id;
-          break;
-        }
-      }
-      map.addSource('province', {
-        'type': 'vector',
-        'url': 'mapbox://zhuwenlong.bpyitk7f'
-      });
-
-      map.addLayer({
-        id: 'province',
-        'source': 'province',
-        'source-layer': 'cn_sheng_polygon',
-        'type': 'fill-extrusion',
-        'minzoom': 1,
-        'maxzoom': 7,
-        'paint': {
-          'fill-extrusion-base': 0,
-          'fill-extrusion-height': ['*', 10000, ["get", 'SHENG_ID']],
-          'fill-extrusion-color': [
-            "match",
-            [
-              "get",
-              "name"
-            ],
-            "湖北",
-            "rgba(204,30,29,1)",
-            "广东",
-            "rgba(239,63,63,1)",
-            "河南",
-            "rgba(239,63,63,1)",
-            "浙江",
-            "rgba(239,63,63,1)",
-            "湖南",
-            "rgba(240,64,64,1)",
-            "安徽",
-            "rgba(240,64,64,1)",
-            "江西",
-            "rgba(241,69,65,1)",
-            "山东",
-            "rgba(247,86,71,1)",
-            "江苏",
-            "rgba(251,98,74,1)",
-            "重庆",
-            "rgba(252,103,75,1)",
-            "四川",
-            "rgba(253,107,76,1)",
-            "黑龙江",
-            "rgba(255,114,80,1)",
-            "北京",
-            "rgba(255,122,86,1)",
-            "上海",
-            "rgba(255,134,96,1)",
-            "河北",
-            "rgba(255,137,99,1)",
-            "福建",
-            "rgba(255,139,101,1)",
-            "广西",
-            "rgba(255,145,106,1)",
-            "陕西",
-            "rgba(255,149,109,1)",
-            "云南",
-            "rgba(255,157,116,1)",
-            "海南",
-            "rgba(255,160,119,1)",
-            "贵州",
-            "rgba(255,162,121,1)",
-            "天津",
-            "rgba(255,162,121,1)",
-            "山西",
-            "rgba(255,162,121,1)",
-            "辽宁",
-            "rgba(255,166,124,1)",
-            "香港",
-            "rgba(255,168,126,1)",
-            "吉林",
-            "rgba(255,168,126,1)",
-            "甘肃",
-            "rgba(255,168,126,1)",
-            "新疆",
-            "rgba(255,175,132,1)",
-            "内蒙古",
-            "rgba(255,175,132,1)",
-            "宁夏",
-            "rgba(255,175,132,1)",
-            "台湾",
-            "rgba(255,191,149,1)",
-            "青海",
-            "rgba(255,199,157,1)",
-            "澳门",
-            "rgba(255,199,157,1)",
-            "西藏",
-            "rgba(255,208,166,1)",
-            "black"
-          ]
-        }
-      }, app.labelLayerId);
-
-      // var me = this;
-      map.on("click", "province", (e) => {
-        if (e.features.length < 1) {
-          return
-        };
-
-        let properties = e.features[0].properties;
-
-        //图层过滤器
-        //map.setFilter(provinceid,["in","SHENG_ID",properties.SHENG_ID])
-
-        let featureinfo = [];
-        Object.keys(properties).map((key) => {
-          let data = {};
-          data["字段"] = key;
-          data["值"] = properties[key];
-          featureinfo.push(data);
-        });
-
-        me.setState({ featuredata: featureinfo, tableshow: true });
-
-      })
-
-    }
 
   }
 
